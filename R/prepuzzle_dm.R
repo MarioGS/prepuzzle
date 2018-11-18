b3be27dfea5dd2a9236d9053c880db888d2cc66a
syneoshealth <mario.gonzalezsales@syneoshealth.com>
2018-11-18 13:12:57 +0100

    dm required

--- R/prepuzzle_dm.R
+++ R/prepuzzle_dm.R
@@ -59,8 +59,16 @@ prepuzzle_dm = function(directory=NULL,
   }
   
   df = df
-  if(lower_case){
+  "%!in%" <- function(x, y) !(x %in% y)
+  required = c("usubjid", "rfstdtc")
+  if (required %!in% names(df) & lower_case == F) {
+    stop("Have you forgotten to set lower_case = T?")
+  }
+  if (lower_case) {
     names(df) = tolower(names(df))
+  }
+  if (required %!in% names(df)) {
+    stop("You need to provide at least the following items: usubjid and rfstdtc")
   }
   
   df$ID = df$usubjid
