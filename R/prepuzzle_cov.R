5e19b4d4353bffd5184795767d69c89fd9779534
syneoshealth <mario.gonzalezsales@syneoshealth.com>
2018-11-18 14:28:51 +0100

    update stop message

--- R/prepuzzle_cov.R
+++ R/prepuzzle_cov.R
@@ -50,7 +50,7 @@ prepuzzle_cov = function(ae=NULL,
              tr=tr,
              vs=vs)
   
-  if(all(lengths(cov) != length(cov[[1]]))){
+  if(all(lengths(cov) == length(cov[[1]]))==FALSE){
     stop("All files have to have the same length. If you have used prepuzzle functions, please make sure include_time argument has been consistently used")
   }
   
