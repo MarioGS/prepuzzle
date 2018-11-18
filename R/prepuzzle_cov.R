b44060c2aa01218185ef8f4a3f2e3f6253008cf0
syneoshealth <mario.gonzalezsales@syneoshealth.com>
2018-11-18 14:19:36 +0100

    cov fastest version

--- R/prepuzzle_cov.R
+++ R/prepuzzle_cov.R
@@ -2,70 +2,101 @@
 #'
 #' @authors Mario Gonzalez Sales
 #'
+#' @param ae dataframe with the adverse event information
+#' @param cm dataframe with the concomitant information
 #' @param dm dataframe with the demographic information
+#' @param dv dataframe with the protocol deviation information 
+#' @param eg dataframe with the electrocardiogram information 
+#' @param is dataframe with the inmunogenicity specimen information 
+#' @param lb dataframe with the laboratory information 
+#' @param ms dataframe with the microbiology specimen information 
+#' @param mb dataframe with the microbiology susceptibility information 
+#' @param ss dataframe with the subject status information 
+#' @param su dataframe with the substance use information 
+#' @param tr dataframe with the tumor results information 
 #' @param vs dataframe with the vital sign information 
 #' @return a dataframe
 #' @export
 #' @examples
 #'
 #'  cov = prepuzzle_cov(dm=dm,
-#'                   vs=vs)
+#'                      vs=vs)
 
-prepuzzle_cov = function(dm=NULL,
-                         vs=NULL,
-                         lower_case = F){
+prepuzzle_cov = function(ae=NULL,
+                         cm=NULL,
+                         dm=NULL,
+                         dv=NULL,
+                         eg=NULL,
+                         is=NULL,
+                         lb=NULL,
+                         mb=NULL,
+                         ms=NULL,
+                         ss=NULL,
+                         su=NULL,
+                         tr=NULL,
+                         vs=NULL){
   
-  packages = c("magrittr","Hmisc","sas7bdat","readr")
-  if (length(setdiff(packages, rownames(installed.packages()))) >
-      0) {
-    install.packages(setdiff(packages, rownames(installed.packages())))
-  }
-  suppressPackageStartupMessages(library("tidyverse"))
-  suppressPackageStartupMessages(library("magrittr"))
-  suppressPackageStartupMessages(library("Hmisc"))
-  suppressPackageStartupMessages(library("sas7bdat"))
-  suppressPackageStartupMessages(library("readr"))
+  cov = list(ae=ae,
+             cm=cm,
+             dm=dm,
+             dv=dv,
+             eg=eg,
+             is=is,
+             lb=lb,
+             mb=mb,
+             ms=ms,
+             su=su,
+             ss=ss,
+             tr=tr,
+             vs=vs)
   
-  
-  if(is.null(dm) & is.null(vs)){
-    stop("Please define at least the dm or the vs arguments")
-  }
-  
-  if(!is.null(dm) & !is.null(vs)){
-    if(length(dm)!=length(vs)){
-      stop("dm and vs has to have the same length. If you have used prepuzzle_dm and prepuzzle_vs, please make sure time_dependent_cov argument has been consistently used")
-    }
-  }
-  
-  
-  if(!is.null(dm)){
-    dm = dm
-    if(lower_case){
-      names(dm) = tolower(names(dm))
-    }
+  if(all(lengths(cov) != length(cov[[1]]))){
+    stop("All files have to have the same length. If you have used prepuzzle functions, please make sure include_time argument has been consistently used")
   }
   
-  if(!is.null(vs)){
-    vs = vs
-    if(lower_case){
-      names(vs) = tolower(names(vs))
-    }
-  }
+  cov = purrr::compact(cov)
   
-  if(!is.null(dm) & !is.null(vs)){
-    df = rbind(dm,vs)
-    df = arrange(df,ID,DATETIME)
-  }
+  df = as.data.frame(data.table::rbindlist(cov))
   
-  if(!is.null(dm) & is.null(vs)){
-    df = dm
-    df = arrange(df,ID,DATETIME)
-  }
-  
-  if(is.null(dm) & !is.null(vs)){
-    df = vs
-    df = arrange(df,ID,DATETIME)
-  }
+  # if(is.null(dm) & is.null(vs)){
+  #   stop("Please define at least the dm or the vs arguments")
+  # }
+  # 
+  # if(!is.null(dm) & !is.null(vs)){
+  #   if(length(dm)!=length(vs)){
+  #     stop("dm and vs has to have the same length. If you have used prepuzzle_dm and prepuzzle_vs, please make sure time_dependent_cov argument has been consistently used")
+  #   }
+  # }
+  # 
+  # 
+  # if(!is.null(dm)){
+  #   dm = dm
+  #   if(lower_case){
+  #     names(dm) = tolower(names(dm))
+  #   }
+  # }
+  # 
+  # if(!is.null(vs)){
+  #   vs = vs
+  #   if(lower_case){
+  #     names(vs) = tolower(names(vs))
+  #   }
+  # }
+  # 
+  # if(!is.null(dm) & !is.null(vs)){
+  #   df = rbind(dm,vs)
+  #   df = arrange(df,ID,DATETIME)
+  # }
+  # 
+  # if(!is.null(dm) & is.null(vs)){
+  #   df = dm
+  #   df = arrange(df,ID,DATETIME)
+  # }
+  # 
+  # if(is.null(dm) & !is.null(vs)){
+  #   df = vs
+  #   df = arrange(df,ID,DATETIME)
+  # }
   
   return(df)
 }
